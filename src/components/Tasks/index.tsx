e452a6ec61318120672aea864746f431df87ba40
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-03-01 18:45:54 -0300

    add extend colors

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -3,15 +3,15 @@ export function Tasks() {
     <section className="w-full max-w-[46rem] mx-auto my-0 mt-[5.625rem] px-4 py-0">
       <header className="flex items-center justify-between mt-[1.5rem]">
         <div className="flex gap-[0.8rem] items-center">
-          <p className="text-[#4ea8de] text-sm font-bold">Tarefas criadas</p>
-          <span className="bg-[#333333] text-[#d9d9d9] px-[9px] py-[3px] rounded-full text-[0.75rem] font-bold">
+          <p className="text-Tblue text-sm font-bold">Tarefas criadas</p>
+          <span className="bg-[#333333] text-Tgray-200 px-[9px] py-[3px] rounded-full text-[0.75rem] font-bold">
             18
           </span>
         </div>
 
         <div className="flex gap-[0.8rem] items-center">
-          <p className="text-[#8284fa] text-sm font-bold">Concluídas</p>
-          <span className="bg-[#333333] text-[#d9d9d9] px-[9px] py-[3px] rounded-full text-[0.75rem] font-bold">
+          <p className="text-Tpurple text-sm font-bold">Concluídas</p>
+          <span className="bg-[#333333] text-Tgray-200 px-[9px] py-[3px] rounded-full text-[0.75rem] font-bold">
             2 de 10
           </span>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Tgray-100": "#f2f2f2",
+        "Tgray-200": "#d9d9d9",
+        "Tgray-300": "#808080",
+        "Tgray-400": "#333333",
+        "Tgray-500": "#262626",
+        "Tgray-600": "#1a1a1a",
+        "Tgray-700": "#0d0d0d",
+        Tpurple: "#8284fa",
+        "Tpurple-dark": "#5e60ce",
+        Tblue: "#4ea8de",
+        "Tblue-dark": "#1e6f9f",
+        Tdanger: "#e25858",
+      },
+    },
   },
   plugins: [],
-}
-
+};
