e221b737dc7a1c623881ef51499cf15fd4d930cd
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-03-02 15:42:19 -0300

    add responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import Logo from "../../assets/toDoLogo.svg";
+import toDoLogo from "../../assets/toDoLogo.svg";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 
 interface Props {
@@ -22,11 +22,11 @@ export function Header({ onAddTask }: Props) {
 
   return (
     <header className="bg-Tgray-700 flex items-center justify-center h-[12.5rem] relative">
-      <img src={Logo} alt="Logo do ToDo App" />
+      <img className="select-none" src={toDoLogo} alt="Logo do ToDo App" />
 
       <form
         onSubmit={handleSubmit}
-        className="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0"
+        className="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] md:w-full md:max-w-[46rem] flex gap-2 md:px-4 md:py-0"
       >
         <input
           required
@@ -35,7 +35,7 @@ export function Header({ onAddTask }: Props) {
           placeholder="Adicione uma nova tarefa"
           className="placeholder:text-Tgray-300 h-full text-lg flex-1 text-Tgray-100 bg-Tgray-500 border border-solid border-Tgray-700 px-4 py-0 rounded-lg"
         />
-        <button className="h-full gap-1 font-bold text-sm px-4 py-0 bg-Tblue-dark text-Tgray-100 border-none rounded-lg flex items-center">
+        <button className="h-full px-2 py-0 gap-1 font-bold text-sm md:px-4 md:py-0 bg-Tblue-dark text-Tgray-100 border-none rounded-lg flex items-center">
           Criar
           <AiOutlinePlusCircle className="font-bold" size={20} />
         </button>
